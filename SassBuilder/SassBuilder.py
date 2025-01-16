1d94a93f58b1230c28ea50fd49c0ffaf5e6fce8d
Lance <lance.wade72@gmail.com>
2025-01-16 11:02:43 -0500

    Fix the decoding issue

--- SassBuilder/SassBuilder.py
+++ SassBuilder/SassBuilder.py
@@ -5,6 +5,7 @@ import os
 
 class SassBuilderCommand(sublime_plugin.WindowCommand):
     def run(self):
+        # Get settings from file.
         settings = sublime.load_settings("SassBuilder.sublime-settings")
         dart_executable = settings.get("dart_sass_path")
         input_dir = settings.get("input", {}).get("dir")
@@ -19,12 +20,17 @@ class SassBuilderCommand(sublime_plugin.WindowCommand):
         try:
             output = subprocess.check_output(
                 compile_command,
-                universal_newlines=True,
+                universal_newlines=False,
                 stderr=subprocess.STDOUT,
                 shell=False
             )
+            # Weird encdoding thing. Let decode.
+            decoded_output = output.decode("utf-8")
             sublime.status_message("Sass compiled successfully!")
         except subprocess.CalledProcessError as e:
+            # Check for encoding
+            error_output = e.output.decode("utf-8")
             sublime.error_message("Sass Builder: " + str(e.output))
         except Exception as e:
+            sublime.error_message("Sass Builder: " + error_output)
             sublime.error_message("Sass Builder: " + str(e))
